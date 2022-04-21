b892b70e13adfcd764dd28157f5113f58a7a946d
Nico Jansen <jansennico@gmail.com>
2022-04-21 16:17:40 +0200

    Reset the demo

--- demos/ts-everywhere/src/main.js
+++ demos/ts-everywhere/src/main.js
@@ -1,9 +1,9 @@
-import fs from "fs";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
 
 /**
- * 
- * @param {string} chapter 
+ *
+ * @param {string} chapter
  */
 async function listSlides(chapter) {
   const entries = await fs.promises.readdir(chapter, { withFileTypes: true });
@@ -15,20 +15,21 @@ async function listSlides(chapter) {
 }
 
 /**
- * @param {string} root 
+ *
+ * @param {string} root
  */
 async function listChapters(root) {
-  const entries = await fs.promises.readdir(root, { withFileTypes: true });
+  const entries = await fs.promises.readdir(root);
   for (const entry of entries) {
     if (entry.isDirectory()) {
       console.log(`# ${entry.name}`);
-      await listSlides(path.resolve(root, entry.name));
+      await listSlides(path.resolve(root, entry));
     }
   }
 }
 
 async function main() {
-  await listChapters("../../slides");
+  listChapters('../../slides');
 }
 
 main().catch((error) => {
